b2d4f0a945b479153ac6f61f552df1b9ffb8fcb7
a.Vienik <alexandrVenik@gmail.com>
2020-09-16 04:08:31 +0300

    Добаляет стили в компонент Button

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button v-on="$listeners" v-bind:type="type">
+  <button v-on="$listeners" v-bind:type="type" :class="{ btn: true, 'btn--outlined': outlined }">
     <slot></slot>
   </button>
 </template>
@@ -11,11 +11,30 @@
       type: {
         type: String,
         default: 'button'
+      },
+      outlined: {
+        type: Boolean,
+        default: false
       }
-    }
+    },
   }
 </script>
 
 <style lang="scss" scoped>
+.btn {
+  display: inline-block;
+  font-size: 18px;
+  background: #FF662D;
+  color: #fff;
+  cursor: pointer;
+  min-width: 220px;
+  border: 1px solid transparent;
+  padding: 8px 15px;
 
+  &--outlined {
+    background: none;
+    border: 1px solid #FF662D;
+    color: #FF662D;
+  }
+}
 </style>
