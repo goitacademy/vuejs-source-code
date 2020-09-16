3959332758cafaa18b29b88ef66b047c47188469
a.Vienik <alexandrVenik@gmail.com>
2020-09-16 04:09:02 +0300

    Добаляет модульные стили в компонент App

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="app">
+  <div :id="$style.app">
     <h1>{{ title }}</h1>
-    <Button @click="increment">Click me</Button>
+    <Button @click="increment" outlined>Click me</Button>
   </div>
 </template>
 
@@ -31,7 +31,7 @@ export default {
 }
 </script>
 
-<style>
+<style module>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
