8d5c047e42c77a262a2a89d747e140ffcfabdb96
a.Vienik <alexandrVenik@gmail.com>
2020-09-16 03:33:30 +0300

    Добавляет компонент Button

---
+++ src/components/Button.vue
@@ -0,0 +1,21 @@
+<template>
+  <button v-bind:type="type">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+  export default {
+    name: 'Button',
+    props: {
+      type: {
+        type: String,
+        default: 'button'
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
