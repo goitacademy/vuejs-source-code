7fb37ec4c668c9339f8bebd9727b26d91b1c6685
a.Vienik <alexandrVenik@gmail.com>
2020-09-23 03:08:31 +0300

    добавляет компонент Container

---
+++ src/components/shared/Container.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'container'
+  }
+</script>
+
+<style lang="scss" scoped>
+.container {
+  max-width: 1110px;
+  margin: 0 auto;
+}
+</style>
