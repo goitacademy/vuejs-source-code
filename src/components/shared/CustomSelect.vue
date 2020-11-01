8ad9e0f20d49c60d9c1e0c8b39bbf593ec63ac9d
a.Vienik <alexandrVenik@gmail.com>
2020-11-02 00:25:24 +0200

    добавляет custom Select

---
+++ src/components/shared/CustomSelect.vue
@@ -0,0 +1,50 @@
+<template>
+  <select v-on="listeners" class="custom-select">
+    <option
+      v-for="item in formatedItems"
+      :key="item.value"
+      :value="item.value"
+    >{{ item.label }}</option>
+  </select>
+</template>
+
+<script>
+  export default {
+    name: 'CustomSelect',
+    props: {
+      items: {
+        type: Array,
+        required: true
+      }
+    },
+    computed: {
+      listeners() {
+        return {
+          ...this.$listeners,
+          input: event => this.$emit('input', event.target.value)
+        }
+      },
+      formatedItems() {
+        return this.items.map(item => {
+          return typeof item === 'object'
+            ? item
+            : { value: item, label: item }
+        })
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/scss/variables';
+
+.custom-select {
+  height: 40px;
+  border: 2px solid $main-color;
+  font-size: 18px;
+  outline: none;
+  padding: 8px 15px;
+  cursor: pointer;
+  display: inline-block;
+}
+</style>
