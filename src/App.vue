f36aa8aeebac3096acfdccc3770bee2792eabffd
a.Vienik <alexandrVenik@gmail.com>
2020-11-19 02:00:23 +0200

    Добавляет хедер и футер

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,28 @@
 <template>
   <div id="app">
-    <h2>{{ text }}</h2>
-    <Container>
-      <ApartmentsFilterForm
-        class="apartments-filter"
-        @submit="filter"
-      />
-    </Container>
-    <p v-if="!filteredApartments.length">Ничего не найдено</p>
-    <ApartmentsList v-else :items="filteredApartments">
-      <template v-slot:apartment="{ apartment }">
-        <ApartmentsItem
-          :key="apartment.id"
-          :descr="apartment.descr"
-          :rating="apartment.rating"
-          :imgSrc="apartment.imgUrl"
-          :price="apartment.price"
-          @click.native="handleItemClick"
-        />
-      </template>
-    </ApartmentsList>
+    <div class="content">
+      <AppHeader />
+      <h2>{{ text }}</h2>
+      <Container>
+        <ApartmentsFilterForm class="apartments-filter" @submit="filter" />
+      </Container>
+      <Container>
+        <p v-if="!filteredApartments.length">Ничего не найдено</p>
+        <ApartmentsList v-else :items="filteredApartments">
+          <template v-slot:apartment="{ apartment }">
+            <ApartmentsItem
+              :key="apartment.id"
+              :descr="apartment.descr"
+              :rating="apartment.rating"
+              :imgSrc="apartment.imgUrl"
+              :price="apartment.price"
+              @click.native="handleItemClick"
+            />
+          </template>
+        </ApartmentsList>
+      </Container>
+    </div>
+    <AppFooter />
   </div>
 </template>
 
@@ -29,6 +32,8 @@ import ApartmentsItem from './components/apartment/ApartmentsItem'
 import apartments from './components/apartment/apartments'
 import ApartmentsFilterForm from './components/apartment/ApartmentsFilterForm'
 import Container from './components/shared/Container'
+import AppFooter from './components/Footer'
+import AppHeader from './components/Header'
 
 export default {
   name: 'App',
@@ -36,7 +41,9 @@ export default {
     ApartmentsList,
     ApartmentsItem,
     ApartmentsFilterForm,
-    Container
+    Container,
+    AppFooter,
+    AppHeader,
   },
   data() {
     return {
@@ -44,14 +51,14 @@ export default {
       apartments,
       filters: {
         city: '',
-        price: 0
-      }
+        price: 0,
+      },
     }
   },
   computed: {
     filteredApartments() {
       return this.filterByCityName(this.filterByPrice(this.apartments))
-    }
+    },
   },
   methods: {
     filter({ city, price }) {
@@ -59,31 +66,36 @@ export default {
       this.filters.price = price
     },
     filterByCityName(apartments) {
-      if(!this.filters.city) return apartments
-      
-      return apartments.filter(apartment => {
+      if (!this.filters.city) return apartments
+
+      return apartments.filter((apartment) => {
         return apartment.location.city === this.filters.city
       })
     },
     filterByPrice(apartments) {
-      if(!this.filters.price) return apartments
+      if (!this.filters.price) return apartments
 
-      return apartments.filter(apartment => {
+      return apartments.filter((apartment) => {
         return apartment.price >= this.filters.price
       })
-    }
-  }
+    },
+  },
 }
 </script>
 
 <style lang="scss" scoped>
 #app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
   font-family: Montserrat, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+.content {
+  flex-grow: 1;
+  padding-top: 120px;
 }
 
 .apartments-filter {
---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer class="footer"></footer>
+</template>
+
+<script>
+  export default {
+    name: 'Footer'
+  }
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/scss/variables.scss';
+
+.footer {
+  background: $secondary-color;
+  height: 30px;
+}
+</style>
